3d340f75d2dae30b0842de729fd3cef8d0756361
kwiatkowskaela <kwiatkowska.grafik@gmail.com>
2017-09-19 21:53:32 +0200

    zmiany grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,26 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    includes: {
-      files: {
-        src: ['html_dev/index.html', 'index.html'], // Source files
-        dest: '',
-        flatten: true,
-        cwd: '.',
-        options: {
-          silent: true
-        }
-      }
-    },
-      watch: {
-        files: ['html_dev/*.html'],
-        tasks: ['includes']
-      }
-  });
+	grunt.initConfig({
+		includes: {
+			files: {
+				src: ['html_dev/*.html', '*.html'], // Source files
+				dest: '',
+				flatten: true,
+				cwd: '.',
+				options: {
+					silent: true
+				}
+			}
+		},
+		watch: {
+			files: ['html_dev/*.html'],
+			tasks: ['includes']
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-includes')
-  grunt.loadNpmTasks('grunt-contrib-watch');;
+	grunt.loadNpmTasks('grunt-includes')
+	grunt.loadNpmTasks('grunt-contrib-watch');;
 
-  grunt.registerTask('default', ['includes', 'watch']);
+	grunt.registerTask('default', ['includes', 'watch']);
 
 };
