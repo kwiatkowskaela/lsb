bf0c700a8585068af0a41061ffe3732c36d37f08
kwiatkowskaela <kwiatkowska.grafik@gmail.com>
2017-09-19 20:57:15 +0200

    opis zmian w danym commicie

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    includes: {
+      files: {
+        src: ['html_dev/index.html', 'index.html'], // Source files
+        dest: '',
+        flatten: true,
+        cwd: '.',
+        options: {
+          silent: true
+        }
+      }
+    },
+      watch: {
+        files: ['html_dev/*.html'],
+        tasks: ['includes']
+      }
+  });
+
+  grunt.loadNpmTasks('grunt-includes')
+  grunt.loadNpmTasks('grunt-contrib-watch');;
+
+  grunt.registerTask('default', ['includes', 'watch']);
+
+};
